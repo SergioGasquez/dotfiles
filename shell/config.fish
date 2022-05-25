8cd12da21ef3d20a306e20f0c276aafff8bf1d43
Sergio Gasquez <sergio.gasquez@gmail.com>
2022-05-25 11:52:24 +0200

    Update dp and add idf44

--- shell/config.fish
+++ shell/config.fish
@@ -12,7 +12,8 @@ alias ....='cd ../../..'
 alias .....='cd ../../../..'
 alias cddoc="cd ~/Documents"
 alias cdesp="cd ~/Documents/Espressif"
-alias cdespdev="cd ~/Documents/Espressif/dev"
+alias cdesprs="cd ~/Documents/Espressif/esp-rs"
+alias cdespidf="cd ~/Documents/Espressif/esp-idf"
 
 # Git
 alias ga='git add'
@@ -51,12 +52,26 @@ alias cdoc='cargo doc --open'
 alias c='cargo'
 # Docker
 alias ds='systemctl start docker'
-alias dp='docker system prune -a'
+alias dp='docker system prune -a -f && docker system prune --volumes -a -f'
 # Esp
 alias cg='cargo generate --git https://github.com/esp-rs/esp-idf-template cargo'
 alias cei='cargo espflash save-image --release'
 alias cef='cargo espflash --release --monitor'
-
+function idf44
+    export IDF_TOOLS_PATH=~/.espressif
+    export IDF_PATH=~/.espressif/frameworks/esp-idf-release-v4.4
+    export IDF_TOOLS_EXPORT_CMD=$IDF_PATH/export.sh
+    export IDF_TOOLS_INSTALL_CMD=$IDF_PATH/install.sh
+    export PIP_USER="no"
+    export LIBCLANG_PATH="/home/sergio/.espressif/tools/xtensa-esp32-elf-clang/esp-14.0.0-20220415-x86_64-unknown-linux-gnu/lib/"
+    set PATH $PATH "/home/sergio/.espressif/tools/xtensa-esp32-elf-clang/esp-14.0.0-20220415-x86_64-unknown-linux-gnu/bin/"
+    set PATH $PATH "/home/sergio/.espressif/tools/xtensa-esp32s2-elf/esp-2021r2-patch3-8.4.0/xtensa-esp32s2-elf/bin/"
+    set PATH $PATH "/home/sergio/.espressif/tools/xtensa-esp32s3-elf/esp-2021r2-patch3-8.4.0/xtensa-esp32s3-elf/bin/"
+    set PATH $PATH "$IDF_PATH/components/esptool_py/esptool"
+    set PATH $PATH "$IDF_PATH/components/espcoredump"
+    set PATH $PATH "$IDF_PATH/components/partition_table"
+    set PATH $PATH "$IDF_PATH/components/app_update"
+end
 
 # Starship
 starship init fish | source
