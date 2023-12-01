1a56bd6184f6954d698bcb1aa0b0765a4ac12f43
Sergio Gasquez <sergio.gasquez@gmail.com>
2023-12-01 09:32:56 +0100

    feat: Avoid hardcoded home

--- windows/shell/Microsoft.PowerShell_profile.ps1
+++ windows/shell/Microsoft.PowerShell_profile.ps1
@@ -15,8 +15,8 @@ Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
 }
 
 # Completions
-. C:\Users\sergi\.dotfiles\windows\shell\espup.ps1
-. C:\Users\sergi\.dotfiles\windows\shell\espflash.ps1
+. $HOME\.dotfiles\windows\shell\espup.ps1
+. $HOME\.dotfiles\windows\shell\espflash.ps1
 
 function Update-Completions {
     espup completions fish > $HOME\.dotfiles\windows\shell\espup.ps1
@@ -34,18 +34,18 @@ Function BackBack { Set-Location -Path .\..\.. }
 Set-Alias -Name ... -Value BackBack
 Function BackBack { Set-Location -Path .\..\..\.. }
 Set-Alias -Name .... -Value BackBack
-Function Documents { Set-Location -Path C:\Users\sergi\Documents }
+Function Documents { Set-Location -Path  $HOME\Documents }
 Set-Alias -Name cddoc -Value Documents
-Function Espressif { Set-Location -Path C:\Users\sergi\Documents\Espressif }
+Function Espressif { Set-Location -Path $HOME\Documents\Espressif }
 Set-Alias -Name cdesp -Value Espressif
-Function EspRs { Set-Location -Path C:\Users\sergi\Documents\Espressif\esp-rs }
+Function EspRs { Set-Location -Path $HOME\Documents\Espressif\esp-rs }
 Set-Alias -Name cdesprs -Value EspRs
-Function Forks { Set-Location -Path C:\Users\sergi\Documents\Espressif\forks }
+Function Forks { Set-Location -Path $HOME\Documents\Espressif\forks }
 Set-Alias -Name cdforks -Value Forks
-Function Tests { Set-Location -Path C:\Users\sergi\Documents\Espressif\tests }
+Function Tests { Set-Location -Path $HOME\Documents\Espressif\tests }
 Set-Alias -Name cdtests -Value Tests
-Function Personal { Set-Location -Path C:\Users\sergi\Documents\Espressif\personal }
+Function Personal { Set-Location -Path $HOME\Documents\Espressif\personal }
 Set-Alias -Name cdper -Value Personal
-Function ThirdParty { Set-Location -Path C:\Users\sergi\Documents\Espressif\third-party }
+Function ThirdParty { Set-Location -Path $HOME\Documents\Espressif\third-party }
 Set-Alias -Name cdtp -Value ThirdParty
 
