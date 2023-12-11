761a8c6b32c81e01985ea60dc2c80f5225594f20
Sergio Gasquez <sergio.gasquez@gmail.com>
2023-12-11 10:52:04 +0100

    feat: Add esprs and fork fish functions

---
+++ common/shell/fork.fish
@@ -0,0 +1,22 @@
+function fork --argument repository_name
+    if test -z $repository_name
+        echo "Please provide a repository name."
+        return 1
+    end
+
+    set repo_path $HOME/Documents/Espressif/forks/$repository_name
+
+    if not test -d $repo_path
+        echo "Cloning https://github.com/SergioGasquez/$repository_name into $repo_path"
+        git clone "https://github.com/SergioGasquez/$repository_name" $repo_path
+        if test $status -ne 0
+            echo "Error: Failed to clone the repository."
+            return 1
+        end
+    else
+        echo "Repository $repository_name already exists in $repo_path"
+    end
+
+    echo "Opening the repository with VS Code"
+    code $repo_path
+end
