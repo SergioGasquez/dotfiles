d233b284cfac31cc30434d6b9217b5f4fa87396a
Sergio Gasquez <sergio.gasquez@gmail.com>
2024-02-21 15:36:14 +0100

    feat: Add sourcing esp-idf alias

--- linux/shell/config.fish
+++ linux/shell/config.fish
@@ -7,10 +7,11 @@ abbr -a upup 'paru -Syua --noconfirm --devel --sudoloop && paru --clean --noconf
 abbr -a vs 'code .'
 abbr -a sandbox 'code $HOME/Documents/Espressif/sandbox'
 abbr -a dotfiles 'code $HOME/.dotfiles'
-## ESP-RS
+# ESP-RS
 export ESPFLASH_BAUD="921600"
 export PATH="/home/sergio/.rustup/toolchains/esp/xtensa-esp-elf/esp-13.2.0_20230928/xtensa-esp-elf/bin:$PATH"
 export LIBCLANG_PATH="/home/sergio/.rustup/toolchains/esp/xtensa-esp32-elf-clang/esp-16.0.4-20231113/esp-clang/lib"
-
+# ESP-IDF
+abbr -a espidf '. /home/sergio/Documents/Espressif/espressif/esp-idf_v5.2/export.fish'
 # Starship
 starship init fish | source
