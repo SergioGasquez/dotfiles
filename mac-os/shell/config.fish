b37f202e0d8fe8ac31cec8bb3b97554bf7284fb9
Sergio Gasquez <sergio.gasquez@gmail.com>
2024-04-08 09:44:47 +0200

    fix: Fix espup exports

--- mac-os/shell/config.fish
+++ mac-os/shell/config.fish
@@ -10,8 +10,8 @@ alias sandbox="code-insiders ~/Documents/Espressif/sandbox"
 alias dotfiles="code-insiders ~/.dotfiles"
 ## ESP-RS
 export ESPFLASH_BAUD="921600"
-export PATH="/home/sergio/.rustup/toolchains/esp/xtensa-esp-elf/esp-13.2.0_20230928/xtensa-esp-elf/bin:$PATH"
-export LIBCLANG_PATH="/home/sergio/.rustup/toolchains/esp/xtensa-esp32-elf-clang/esp-16.0.4-20231113/esp-clang/lib"
+export PATH="/Users/sergio/.rustup/toolchains/esp/xtensa-esp-elf/esp-13.2.0_20230928/xtensa-esp-elf/bin:$PATH"
+export LIBCLANG_PATH="/Users/sergio/.rustup/toolchains/esp/xtensa-esp32-elf-clang/esp-16.0.4-20231113/esp-clang/lib"
 
 # Brew
 eval (/opt/homebrew/bin/brew shellenv)
