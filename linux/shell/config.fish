36b6936864aba2d912058e753334d0e9e3f7361a
Sergio Gasquez <sergio.gasquez@gmail.com>
2024-06-27 10:06:38 +0200

    feat: Update libclang_path

--- linux/shell/config.fish
+++ linux/shell/config.fish
@@ -10,7 +10,7 @@ abbr -a dotfiles 'code $HOME/.dotfiles'
 # ESP-RS
 export ESPFLASH_BAUD="921600"
 export PATH="/home/sergio/.rustup/toolchains/esp/xtensa-esp-elf/esp-13.2.0_20230928/xtensa-esp-elf/bin:$PATH"
-export LIBCLANG_PATH="/home/sergio/.rustup/toolchains/esp/xtensa-esp32-elf-clang/esp-16.0.4-20231113/esp-clang/lib"
+export LIBCLANG_PATH="/home/sergio/.rustup/toolchains/esp/xtensa-esp32-elf-clang/esp-17.0.1_20240419/esp-clang/lib"
 # ESP-IDF
 abbr -a espidf '. /home/sergio/Documents/Espressif/espressif/esp-idf_v5.2/export.fish'
 # Starship
