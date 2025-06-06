323c179bd0d4fb1b6321ac2f7ffb2a0edf271f2d
Sergio Gasquez <sergio.gasquez@gmail.com>
2025-06-06 08:57:56 +0200

    feat: Update MacOs fish config and Rust aliases

--- mac-os/shell/config.fish
+++ mac-os/shell/config.fish
@@ -2,7 +2,7 @@
 # Commmon aliases
 . ~/.config/fish/aliases.fish
 # Brew
-alias upup="brew upgrade && cargo install-update -a && rustup update && rustup self update && espup update"
+alias upup="brew upgrade --cask $(brew list --cask | grep -v 'ftdi-vcp-driver')  && cargo install-update -a && rustup update && rustup self update && espup update"
 # Editor
 alias vs="code-insiders ."
 alias code="code-insiders"
@@ -10,8 +10,8 @@ alias sandbox="code-insiders ~/Documents/Espressif/sandbox"
 alias dotfiles="code-insiders ~/.dotfiles"
 ## ESP-RS
 export ESPFLASH_BAUD="921600"
-export PATH="/Users/sergio/.rustup/toolchains/esp/xtensa-esp-elf/esp-13.2.0_20230928/xtensa-esp-elf/bin:$PATH"
-export LIBCLANG_PATH="/Users/sergio/.rustup/toolchains/esp/xtensa-esp32-elf-clang/esp-16.0.4-20231113/esp-clang/lib"
+export PATH="/Users/sergio/.rustup/toolchains/esp/xtensa-esp-elf/esp-14.2.0_20240906/xtensa-esp-elf/bin:$PATH"
+export LIBCLANG_PATH="/Users/sergio/.rustup/toolchains/esp/xtensa-esp32-elf-clang/esp-19.1.2_20250225/esp-clang/lib"
 
 # Brew
 eval (/opt/homebrew/bin/brew shellenv)
@@ -22,3 +22,6 @@ ssh-add $HOME/.ssh/id_ed25519 >/dev/null 2>&1
 
 # Starship
 starship init fish | source
+
+# uv
+fish_add_path "/Users/sergio/.local/bin"
