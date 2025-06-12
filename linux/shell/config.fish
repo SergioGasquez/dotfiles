1f7311c48cab2a49eae2ca5aee006c0ba2496523
Sergio Gasquez <sergio.gasquez@gmail.com>
2025-06-12 16:05:54 +0200

    feat: Update pacakges

--- linux/shell/config.fish
+++ linux/shell/config.fish
@@ -10,6 +10,7 @@ abbr -a dotfiles 'cursor $HOME/.dotfiles'
 # ESP-RS
 export ESPFLASH_BAUD="921600"
 export PATH="/home/sergio/.rustup/toolchains/esp/xtensa-esp-elf/esp-14.2.0_20240906/xtensa-esp-elf/bin:$PATH"
+export PATH="/home/sergio/.local/bin:$PATH"
 export LIBCLANG_PATH="/home/sergio/.rustup/toolchains/esp/xtensa-esp32-elf-clang/esp-18.1.2_20240912/esp-clang/lib"
 # ESP-IDF
 abbr -a espidf '. /home/sergio/Documents/Espressif/espressif/esp-idf_v5.2/export.fish'
